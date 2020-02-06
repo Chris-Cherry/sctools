6fafc3997ab316a5c0b2d375ef673bccee1434fa
Chris-Cherry <ccherry.6574@gmail.com>
2020-02-06 16:02:45 -0500

    Updating GSE score

--- R/make_gse_scores.R
+++ R/make_gse_scores.R
@@ -16,154 +16,55 @@
 #' @return  Outputs seurat object
 #' @export
 
-make_gse_scores <- function(ser, directory = NULL, out_dir = '3_gse', from_gene = 'HGNC', to_gene = 'MGI', 
-    subset_gse = NULL, csv_dir = NULL, type = 'Real'){
+make_gse_scores <- function(ser, directory = NULL, out_dir = '3_gse', 
+    from_gene = 'HGNC', to_gene = 'MGI', subset_gse = NULL, csv_dir = NULL, 
+    type = 'Real'){
     
     dir.create(out_dir)
-
-    if (!is.null(subset_gse) || !is.null(csv_dir) || !is.null(directory)){
-        if (!is.null(csv_dir)){
-            # Red in an excel file
-            gse_conv = read.table(csv_dir, sep = ',')
-            for (i in 1:length(gse_conv)){
-                tmp = gse_conv[i]
-                tmp = tmp[tmp != ""]
-                if (i == 1){
-                    gse <- list(t(tmp))
-                }
-                else{
-                    gse[i] <- list(t(tmp))
-                }
-            }
-            
-            # Extract the Seurat data
-            gse_ser = ser@assays$RNA@scale.data
-            gse_ser_row = rownames(gse_ser)
-            gse_ser_col = colnames(gse_ser)
-
-            for (i in 1:length(gse)){
-                # Find unique genes
-                gse = unique(gse)
-                # Convert genes
-                tmp = convert_genes(gse[[i]], from = from_gene, to = to_gene)
-                tmp = tmp[,2]
-
-                # Find index of each gene set
-                idx_gse = match(tmp, gse_ser_row)
-
-                idx_gse = na.omit(idx_gse)
-
-                gse_ser_sub = gse_ser[idx_gse,]
-                gse_ser_tmp <- Matrix::colSums(gse_ser_sub)
-                if (i == 1){
-                    gse_ser_set = gse_ser_tmp
-                    set_name = paste0("GeneSet", i)
-                }
-                else{
-                    gse_ser_set = rbind(gse_ser_set, gse_ser_tmp)
-                    set_name[i] = paste0("GeneSet", i)
-                }
-            }
+    gse = list()
+    # Read in gene sets
+    if (!is.null(subset_gse)){
+        gse = c(gse, subset_gse)
+    } 
+    if (!is.null(directory)){
+        for(file in directory){
+            gse = c(gse, fgsea::gmtPathways(file))
         }
-        else if (!is.null(directory)){
-            # Read in geneset with gmt pathway
-            gse = fgsea::gmtPathways(directory)
-
-            # Extract the Seurat data
-            gse_ser = ser@assays$RNA@scale.data
-            gse_ser_row = rownames(gse_ser)
-            gse_ser_col = colnames(gse_ser)
-
-            for (i in 1:length(gse)){
-                # Remove empty cells
-                gse = gse[gse != ""]
-                # Find unique genes
-                gse = unique(gse)
-                # Convert genes
-                tmp = convert_genes(gse[[i]], from = from_gene, to = to_gene)
-                tmp = tmp[,2]
-
-                # Find index of each gene set
-                idx_gse = match(tmp, gse_ser_row)
-
-                idx_gse = na.omit(idx_gse)
-
-                gse_ser_sub = gse_ser[idx_gse,]
-                gse_ser_tmp <- Matrix::colSums(gse_ser_sub)
-                if (i == 1){
-                    gse_ser_set = gse_ser_tmp
-                    set_name = paste0("GeneSet", i)
-                }
-                else{
-                    gse_ser_set = rbind(gse_ser_set, gse_ser_tmp)
-                    set_name[i] = paste0("GeneSet", i)
-                }
-            }
+    } 
+    if (!is.null(csv_dir)){
+        table = read.table(csv_dir, sep = ',', header = TRUE, stringsAsFactors = FALSE)
+        for(col in 1:ncol(table)){
+            gse[[table[1,col]]] = table[-1, col]
         }
-        else{
-            if (typeof(subset_gse) =='list'){
-            # Extract the Seurat data
-            gse = subset_gse
-            gse_ser = ser@assays$RNA@scale.data
-            gse_ser_row = rownames(gse_ser)
-            gse_ser_col = colnames(gse_ser)
-
-            for (i in 1:length(gse)){
-                # Remove empty cells
-                gse = gse[gse != ""]
-                # Find unique genes
-                gse = unique(gse)
-                # Convert genes
-                tmp = convert_genes(gse[[i]], from = from_gene, to = to_gene)
-                tmp = tmp[,2]
-
-                # Find index of each gene set
-                idx_gse = match(tmp, gse_ser_row)
-
-                idx_gse = na.omit(idx_gse)
-
-                gse_ser_sub = gse_ser[idx_gse,]
-                gse_ser_tmp <- Matrix::colSums(gse_ser_sub)
-                if (i == 1){
-                    gse_ser_set = gse_ser_tmp
-                    set_name = paste0("GeneSet", i)
-                }
-                else{
-                    gse_ser_set = rbind(gse_ser_set, gse_ser_tmp)
-                    set_name[i] = paste0("GeneSet", i)
-                }
-                }
-            }
-            else{
-                print("Please insert the right data type (only except list data type)")
-            }
-        }
-
-        rownames(gse_ser_set) = set_name
-
-        if (type != "Real" && type == "Abs")
-        {
-            gse_ser_set = abs(gse_ser_set)
-        }
-        else{
-            print("Default data type is Real values")
-        }
-
-        ser[['GeneSet']] = Seurat::CreateAssayObject(gse_ser_set)
-
-        #Make feature plots
-        tmp = ser
-        DefaultAssay(tmp) = 'GeneSet'
-
-        pdf(paste0(out_dir, '/FeaturePlot_GeneSet.pdf'), height = 20, width = 20)
-        print(Seurat::FeaturePlot(tmp, features = rownames(tmp), ncol = 3))
     }
 
-    else{
-        print("Please specify directory of the geneset, or list of gene set that you want to analyse")
+    # Get scale data and convert genes if necessary
+    scale_data = Seurat::GetAssayData(ser, slot = 'scale.data')
+    if(to_gene != from_gene){
+        gene_conv = convert_genes(rownames(scale_data), to_gene, from_gene)
+        for(set in names(gse)){
+            g = gse[[set]]
+            mid = match(g, gene_conv[,2])
+            mid = na.omit(mid)
+            gse[[set]] = gene_conv[mid, 1]
+        }
     }
 
-    dev.off()
+    # Score gene sets
+    set_scores = matrix(0, ncol = ncol(scale_data), nrow = length(gse))
+    rownames(set_scores) = names(gse)
+    colnames(set_scores) = colnames(scale_data) 
+    for(set in names(gse)){
+        if(length(gse[[set]]) == 1){next}
+        m = scale_data[gse[[set]],]
+        scores = Matrix::colMeans(m)
+        set_scores[set, names(scores)] = scores
+    }
 
+    # Add scores to Seurat object
+    if(sum(names(ser@assays) == 'set_scores')){
+        set_scores = rbind(ser@assays$set_scores@counts, set_scores)
+    }
+    ser[['set_scores']] = Seurat::CreateAssayObject(set_scores)
     return(ser)
-}
+}  
